dbc8a3cea8046d8bb3b007f40f5237d6605d3658
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-26 22:54:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -50,8 +50,9 @@ class Dog
   
   def self.find_by_id(id) 
     sql = "SELECT * FROM dogs WHERE id = ?"
- 
-    DB[:conn].execute(sql, self.id)
+    result = DB[:conn].execute(sql, self.id)[0]
+    binding.pry
+    Dogs.new(result[0], result[1], result[2])
   end 
   
   def self.find_or_create_by
