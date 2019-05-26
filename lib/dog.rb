5db407e8066725bcb677183f235a0d008b6afd8c
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-26 22:21:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,7 +37,7 @@ class Dog
       dog
     else 
       sql = <<-SQL
-        UPDATE dogs (name, breed)
+        UPDATE dogs SET 
         VALUES (?, ?)
       SQL
       dog = DB[:conn].execute(sql, self.name, self.breed)
