6c6114d042b7a815cbee02c19ae30ad712ddbc48
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-26 22:58:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,8 +54,8 @@ class Dog
     Dog.new(id: result[0], name: result[1], breed: result[2])
   end 
   
-  def self.find_or_create_by
-  
+  def self.find_or_create_by(name: ,breed:)
+    
   end 
   
   def self.new_from_db
