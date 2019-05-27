859bad9cfdc5ff4ecc35fa92690c2fda88642729
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-05-27 00:11:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,8 +66,7 @@ class Dog
   end 
   
   def self.new_from_db(row)
-    sql = "SELECT * FROM dogs"
-    DB[:conn].execute(sql, name, breed)
+    Dog.new(id: row[0], name: row[1], breed: row[2])
   end 
   
   def self.find_by_name
